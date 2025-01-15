f72287f2f76bfd5a051365b3f808f73c11115f0f
Jars <maulidfajar163@gmail.com>
2025-01-15 21:48:20 +0700

    menambahkan file project dan membuat web hingga function ubah warna button & gambar

---
+++ main.js
@@ -0,0 +1,16 @@
+function setTidurLed() {
+  ledTidur.style.backgroundColor = "#579fff";
+  tidurLedImage.src = "./assets/led-on.png";
+}
+function setTamuLed() {
+  ledTamu.style.backgroundColor = "#579fff";
+  tamuLedImage.src = "./assets/led-on.png";
+}
+function setMakanLed() {
+  ledMakan.style.backgroundColor = "#579fff";
+  makanLedImage.src = "./assets/led-on.png";
+}
+function setKeluargaLed() {
+  ledKeluarga.style.backgroundColor = "#579fff";
+  keluargaLedImage.src = "./assets/led-on.png";
+}
