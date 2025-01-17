b09aed070c2532e5accc57d9774717183676e40f
Jars <maulidfajar163@gmail.com>
2025-01-17 23:09:00 +0700

    memindah file esp, update js styling

--- script.js
+++ script.js
@@ -2,88 +2,108 @@ const endpoint = "http://192.168.1.17";
 
 function getTidurLed() {
   fetch(endpoint + "/tidur", {
-    method: "GET"
-  }).then(response => response.text()).then(result => {
-    if(result == "ON") {
-      ledTidur.style.backgroundColor = "#579fff";
-      tidurLedImage.src = "./assets/led-on.png";
-    } else {
-      ledTidur.style.backgroundColor = "red";
-      tidurLedImage.src = "./assets/led-off.png";
-    }
-  });
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        ledTidur.style.backgroundColor = "#579fff";
+        ledTidur.innerHTML = "TURN OFF";
+        tidurLedImage.src = "./assets/led-on.png";
+      } else {
+        ledTidur.style.backgroundColor = "red"; 
+        tidurLedImage.src = "./assets/led-off.png";
+      }
+    });
 }
 
 function getTamuLed() {
   fetch(endpoint + "/tamu", {
-    method: "GET"
-  }).then(response => response.text()).then(result => {
-    if(result == "ON") {
-      ledTamu.style.backgroundColor = "#579fff";
-      tamuLedImage.src = "./assets/led-on.png";
-    } else {
-      ledTamu.style.backgroundColor = "red";
-      tamuLedImage.src = "./assets/led-off.png";
-    }
-  });
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        ledTamu.style.backgroundColor = "#579fff";
+        tamuLedImage.src = "./assets/led-on.png";
+        ledTamu.innerHTML = "TURN OFF";
+      } else {
+        ledTamu.style.backgroundColor = "red";
+        tamuLedImage.src = "./assets/led-off.png";
+      }
+    });
 }
 
 function getMakanLed() {
   fetch(endpoint + "/makan", {
-    method: "GET"
-  }).then(response => response.text()).then(result => {
-    if(result == "ON") {
-      ledMakan.style.backgroundColor = "#579fff";
-      makanLedImage.src = "./assets/led-on.png";
-    } else {
-      ledTidur.style.backgroundColor = "red";
-      makanLedImage.src = "./assets/led-off.png";
-    }
-  });
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        ledMakan.style.backgroundColor = "#579fff";
+        ledMakan.innerHTML = "TURN OFF";
+        makanLedImage.src = "./assets/led-on.png";
+      } else {
+        ledTidur.style.backgroundColor = "red";
+        makanLedImage.src = "./assets/led-off.png";
+      }
+    });
 }
 
 function getKeluargaLed() {
   fetch(endpoint + "/keluarga", {
-    method: "GET"
-  }).then(response => response.text()).then(result => {
-    if(result == "ON") {
-      ledKeluarga.style.backgroundColor = "#579fff";
-      keluargaLedImage.src = "./assets/led-on.png";
-    } else {
-      ledKeluarga.style.backgroundColor = "red";
-      keluargaLedImage.src = "./assets/led-off.png";
-    }
-  });
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        ledKeluarga.style.backgroundColor = "#579fff";
+        ledKeluarga.innerHTML = "TURN OFF";
+        keluargaLedImage.src = "./assets/led-on.png";
+      } else {
+        ledKeluarga.style.backgroundColor = "red";
+        keluargaLedImage.src = "./assets/led-off.png";
+      }
+    });
 }
 
 function setTidurLed() {
   fetch(endpoint + "/tidur", {
-    method: "POST"
-  }).then(response => response.text()).then(result => location.reload())
+    method: "POST",
+  })
+    .then((response) => response.text())
+    .then((result) => location.reload());
 
   ledTidur.style.backgroundColor = "#579fff";
   tidurLedImage.src = "./assets/led-on.png";
 }
 function setTamuLed() {
   fetch(endpoint + "/tamu", {
-    method: "POST"
-  }).then(response => response.text()).then(result => location.reload())
+    method: "POST",
+  })
+    .then((response) => response.text())
+    .then((result) => location.reload());
 
   ledTamu.style.backgroundColor = "#579fff";
   tamuLedImage.src = "./assets/led-on.png";
 }
 function setMakanLed() {
   fetch(endpoint + "/makan", {
-    method: "POST"
-  }).then(response => response.text()).then(result => location.reload())
+    method: "POST",
+  })
+    .then((response) => response.text())
+    .then((result) => location.reload());
 
   ledMakan.style.backgroundColor = "#579fff";
   makanLedImage.src = "./assets/led-on.png";
 }
 function setKeluargaLed() {
   fetch(endpoint + "/keluarga", {
-    method: "POST"
-  }).then(response => response.text()).then(result => location.reload())
+    method: "POST",
+  })
+    .then((response) => response.text())
+    .then((result) => location.reload());
 
   ledKeluarga.style.backgroundColor = "#579fff";
   keluargaLedImage.src = "./assets/led-on.png";
@@ -92,4 +112,4 @@ function setKeluargaLed() {
 getTidurLed();
 getTamuLed();
 getMakanLed();
-// getKeluarga();
+getKeluargaLed();
